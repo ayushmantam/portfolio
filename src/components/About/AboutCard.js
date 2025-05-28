52753aa44431e7cb2d22b3eb179818e5cd44b610
ayushman_tamrakar <ayushmantamrakar1nitb@gmail.com>
2025-05-29 03:14:20 +0530

    about page changes

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -11,7 +11,7 @@ function AboutCard() {
             Hi Everyone, I am <span className="purple">Ayushman Tamrakar </span>
             from <span className="purple"> Bhopal, India.</span>
             <br />
-            I am currently pursuing a B.Tech in Mathematics and Data Science from NIT Bhopal, and I am in my 3rd year.
+            I am currently pursuing a B.Tech in Mathematics and Data Science from NIT Bhopal, and I am in my Final year (2026).
             <br />
 
             <br />
@@ -20,8 +20,8 @@ function AboutCard() {
           </p>
 
           <p>⚡ I have studied core software engineering subjects like Data Structures, Algorithms, DBMS, Operating Systems, Computer Networks, etc.</p>
-          <p>⚡ In competitive programming, I have achieved a max rating of 1406 on Codeforces <span className="purple">(Specialist)</span> and a max rating of 1630 on CodeChef (3 Star). I have solved over 380 problems on Codeforces and more than 560 problems on LeetCode, with a max rating of 1718.</p>
-          <p>⚡ Some of my notable achievements include securing Global Rank 437 in CodeChef Starters Div3 and 318 in CodeChef Starters Div4.</p>
+          <p>⚡ In competitive programming, I have achieved a max rating of 1468 on Codeforces <span className="purple">(Specialist)</span> and a max rating of 1768 on CodeChef (3 Star). I have solved over 560 problems on Codeforces and more than 660 problems on LeetCode, with a max rating of 1718.</p>
+          <p>⚡ Some of my notable achievements include securing Global Rank 235 in CodeChef Starters Div2 and 1404 in Codeforces Div2 Div3.</p>
 
 
         </blockquote>
